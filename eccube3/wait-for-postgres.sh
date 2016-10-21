5fca95d20312f24379c8b2107b56a33fb106d1e6
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2016-10-21 19:55:05 +0900

    docker-compose で動かす試み

---
+++ eccube3/wait-for-postgres.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+host="$1"
+shift
+cmd="$@"
+
+export PGPASSWORD=password
+export DBSERVER=$host
+
+until psql -h "$host" -U "cube3_dev_user" -d "template1" -c '\l'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - executing command"
+/var/www/exec_env.sh
+php /var/www/eccube_install.php pgsql none --skip-createdb --verbose
+chown -R www-data:www-data /var/www
+apache2-foreground
