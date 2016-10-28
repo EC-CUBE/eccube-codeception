00c9e82770040e30e6464c738f1f7595c40000b9
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2016-10-28 17:42:05 +0900

    MySQL にも対応

--- eccube3/wait-for-pgsql.sh
+++ eccube3/wait-for-pgsql.sh
@@ -2,15 +2,13 @@
 
 set -e
 
-host="$1"
-shift
 cmd="$@"
 
 export PGPASSWORD=password
-export DBSERVER=$host
+export DBSERVER=db
 export AUTH_MAGIC=XjosAXOzO1B3mE0egwQA
 
-until psql -h "$host" -U "cube3_dev_user" -d "template1" -c '\l'; do
+until psql -h db -U "cube3_dev_user" -d "template1" -c '\l'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
