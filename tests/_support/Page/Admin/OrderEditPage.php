1f4fed825ef4b74eabe7b69e210570acbd084bc4
kiy0taka <kiy0taka333@gmail.com>
2016-11-11 18:31:33 +0900

    travis上で失敗するのを調整

--- tests/_support/Page/Admin/OrderEditPage.php
+++ tests/_support/Page/Admin/OrderEditPage.php
@@ -137,7 +137,9 @@ class OrderEditPage extends AbstractAdminPage
 
     public function 商品検索($value = '')
     {
+        $this->tester->executeJS('window.scrollTo(0, 900);');
         $this->tester->click('#aside_wrap > form > div > div:nth-child(3) > div.box-body.accpanel > div > div.btn_area > ul > li:nth-child(1) > a');
+        $this->tester->waitForElement(['id' => 'search_product_modal_box']);
         $this->tester->fillField(['id' => 'admin_search_product_id'], $value);
         $this->tester->click('#searchProductModalButton');
         return $this;
