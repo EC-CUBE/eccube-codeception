c938dab3dfa3967b1c1f681045dad5ae999cef2e
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2016-11-14 19:31:44 +0900

    インストール順序でコケるのを修正

--- tests/acceptance/EA09PluginsCest.php
+++ tests/acceptance/EA09PluginsCest.php
@@ -18,53 +18,53 @@ class EA09PluginsCest
     public function _before(\AcceptanceTester $I)
     {
         $this->plugins = [
-            [
+            1 => [
                 'code' => 'Coupon',
                 'file' => 'Coupon_v1.0.3.tar.gz',
                 'url' => 'https://github.com/EC-CUBE/coupon-plugin/releases/download/1.0.3/Coupon_v1.0.3.tar.gz'
             ],
-            [
+            2 => [
                 'code' => 'Maker',
                 'file' => 'Maker_v0.0.2.tar.gz',
                 'url' => 'https://github.com/EC-CUBE/maker-plugin/releases/download/0.0.2/Maker_v0.0.2.tar.gz'
             ],
-            [
+            3 => [
                 'code' => 'Recommend',
                 'file' => 'Recommend_v1.0.3.tar.gz',
                 'url' => 'https://github.com/EC-CUBE/Recommend-plugin/releases/download/1.0.3/Recommend_v1.0.3.tar.gz'
             ],
-            // [
+            // 4 => [
             //     'code' => 'SalesReport',
             //     'file' => 'SalesReport_v0.0.2.tar.gz',
             //     'url' => 'https://github.com/EC-CUBE/sales-report-plugin/releases/download/0.0.2/SalesReport_v0.0.2.tar.gz'
 
             // ],
-            // [
+            // 5 => [
             //     'code' => 'RelatedProduct',
             //     'file' => 'RelatedProduct_v0.0.2.tar.gz',
             //     'url' => 'https://github.com/EC-CUBE/related-product-plugin/releases/download/0.0.2/RelatedProduct_v0.0.2.tar.gz'
             // ],
-            // [
+            // 6 => [
             //     'code' => 'MailMaga',
             //     'file' => 'MailMaga_v0.0.3.tar.gz',
             //     'url' => 'https://github.com/EC-CUBE/mail-magazine-plugin/releases/download/0.0.3/MailMaga_v0.0.3.tar.gz'
             // ],
-            // [
+            // 7 => [
             //     'code' => 'EccubeApi',
             //     'file' => 'EccubeApi-1.0.2.tar.gz',
             //     'url' => 'https://github.com/EC-CUBE/eccube-api/releases/download/v0-beta-1.0.2/EccubeApi-1.0.2.tar.gz'
             // ],
-            // [
+            // 8 => [
             //     'code' => 'Point',
             //     'file' => 'Point_v1.0.0.tar.gz',
             //     'url' => 'https://github.com/EC-CUBE/point-plugin/releases/download/1.0.0/Point_v1.0.0.tar.gz'
             // ],
-            // [
+            // 9 => [
             //     'code' => 'OrderPdf',
             //     'file' => 'OrderPdf_v0.0.2.tar.gz',
             //     'url' => 'https://github.com/EC-CUBE/order-pdf-plugin/releases/download/0.0.2/OrderPdf_v0.0.2.tar.gz'
             // ],
-            // [
+            // 10 => [
             //     'code' => 'ListingAdCsv',
             //     'file' => 'ListingAdCsv_20160204.tar.gz',
             //     'url' => 'https://github.com/EC-CUBE/listing-ad-plugin/releases/download/v.1.0.1/ListingAdCsv_20160204.tar.gz'
