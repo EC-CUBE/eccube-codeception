f5aaf97877ba46a91c8f705c60696c19b218da4e
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2016-11-15 19:07:45 +0900

    Fix .gitignore

---
+++ tests/_data/plugin_fixtures.php
@@ -0,0 +1,68 @@
+<?php
+/*
+ * インストールするプラグインの一覧
+ * ここに定義したプラグインが、独自プラグインとしてインストールされる。
+ * プラグイン名(code ではないのに注意)昇順に羅列すること。
+ */
+return [
+    1 => [
+        'name' => 'Coupon',
+        'code' => 'Coupon',
+        'file' => 'Coupon_v1.0.3.tar.gz',
+        'url' => 'https://github.com/EC-CUBE/coupon-plugin/releases/download/1.0.3/Coupon_v1.0.3.tar.gz'
+    ],
+    2 => [
+        'name' => 'EccubeApi',
+        'code' => 'EccubeApi',
+        'file' => 'EccubeApi-1.0.2.tar.gz',
+        'url' => 'https://github.com/EC-CUBE/eccube-api/releases/download/v0-beta-1.0.2/EccubeApi-1.0.2.tar.gz'
+    ],
+    3 => [
+        'name' => 'MailMagazine',
+        'code' => 'MailMaga',
+        'file' => 'MailMaga_v0.0.3.tar.gz',
+        'url' => 'https://github.com/EC-CUBE/mail-magazine-plugin/releases/download/0.0.3/MailMaga_v0.0.3.tar.gz'
+    ],
+    4 => [
+        'name' => 'Maker',
+        'code' => 'Maker',
+        'file' => 'Maker_v0.0.2.tar.gz',
+        'url' => 'https://github.com/EC-CUBE/maker-plugin/releases/download/0.0.2/Maker_v0.0.2.tar.gz'
+    ],
+    5 => [
+        'name' => 'OrderPdf',
+        'code' => 'OrderPdf',
+        'file' => 'OrderPdf_v0.0.2.tar.gz',
+        'url' => 'https://github.com/EC-CUBE/order-pdf-plugin/releases/download/0.0.2/OrderPdf_v0.0.2.tar.gz'
+    ],
+    6 => [
+        'name' => 'Pointプラグイン',
+        'code' => 'Point',
+        'file' => 'Point_v1.0.0.tar.gz',
+        'url' => 'https://github.com/EC-CUBE/point-plugin/releases/download/1.0.0/Point_v1.0.0.tar.gz'
+    ],
+    7 => [
+        'name' => 'おすすめ商品管理プラグイン',
+        'code' => 'Recommend',
+        'file' => 'Recommend_v1.0.3.tar.gz',
+        'url' => 'https://github.com/EC-CUBE/Recommend-plugin/releases/download/1.0.3/Recommend_v1.0.3.tar.gz'
+    ],
+    8 => [
+        'name' => '売上集計プラグイン',
+        'code' => 'SalesReport',
+        'file' => 'SalesReport_v0.0.2.tar.gz',
+        'url' => 'https://github.com/EC-CUBE/sales-report-plugin/releases/download/0.0.2/SalesReport_v0.0.2.tar.gz'
+    ],
+    9 => [
+        'name' => 'リスティング広告入稿用CSV作成プラグイン',
+        'code' => 'ListingAdCsv',
+        'file' => 'ListingAdCsv_20160204.tar.gz',
+        'url' => 'https://github.com/EC-CUBE/listing-ad-plugin/releases/download/v.1.0.1/ListingAdCsv_20160204.tar.gz'
+    ],
+    10 => [
+        'name' => '関連商品プラグイン',
+        'code' => 'RelatedProduct',
+        'file' => 'RelatedProduct_v0.0.2.tar.gz',
+        'url' => 'https://github.com/EC-CUBE/related-product-plugin/releases/download/0.0.2/RelatedProduct_v0.0.2.tar.gz'
+    ],
+];
