a852e1b85bf45cfda904e6d93ae9d79af2774851
kiy0taka <kiy0taka333@gmail.com>
2017-05-11 12:04:39 +0900

    新しいレイアウト管理に対応

--- tests/_support/Page/Admin/PageManagePage.php
+++ tests/_support/Page/Admin/PageManagePage.php
@@ -26,21 +26,15 @@ class PageManagePage extends AbstractAdminPage
         $this->tester->click('#main > div > div > div > div.row.btn_area2 > div > a');
     }
 
-    public function レイアウト編集($pageName)
-    {
-        $this->tester->click(['xpath' => "//*[@id='sortable_list_box__list']//div[@class='item_box tr']/div[@class='item_pattern td'][contains(text(),'${pageName}')]/parent::node()/div[@class='icon_edit td']/div/a"]);
-        $this->tester->click(['xpath' => "//*[@id='sortable_list_box__list']//div[@class='item_box tr']/div[@class='item_pattern td'][contains(text(),'${pageName}')]/parent::node()/div[@class='icon_edit td']/div/ul/li[1]/a"]);
-    }
-
     public function ページ編集($pageName)
     {
-        $this->tester->click(['xpath' => "//*[@id='sortable_list_box__list']//div[@class='item_box tr']/div[@class='item_pattern td'][contains(text(),'${pageName}')]/parent::node()/div[@class='icon_edit td']/div/a"]);
-        $this->tester->click(['xpath' => "//*[@id='sortable_list_box__list']//div[@class='item_box tr']/div[@class='item_pattern td'][contains(text(),'${pageName}')]/parent::node()/div[@class='icon_edit td']/div/ul/li[2]/a"]);
+        $this->tester->click(['xpath' => "//*[@id='sortable_list_box__list']//div[@class='item_box tr']/div[@class='item_pattern td']/a[contains(text(),'${pageName}')]/parent::node()/parent::node()/div[@class='icon_edit td']/div/a"]);
+        $this->tester->click(['xpath' => "//*[@id='sortable_list_box__list']//div[@class='item_box tr']/div[@class='item_pattern td']/a[contains(text(),'${pageName}')]/parent::node()/parent::node()/div[@class='icon_edit td']/div/ul/li[1]/a"]);
     }
 
     public function 削除($pageName)
     {
-        $this->tester->click(['xpath' => "//*[@id='sortable_list_box__list']//div[@class='item_box tr']/div[@class='item_pattern td'][contains(text(),'${pageName}')]/parent::node()/div[@class='icon_edit td']/div/a"]);
-        $this->tester->click(['xpath' => "//*[@id='sortable_list_box__list']//div[@class='item_box tr']/div[@class='item_pattern td'][contains(text(),'${pageName}')]/parent::node()/div[@class='icon_edit td']/div/ul/li[3]/a"]);
+        $this->tester->click(['xpath' => "//*[@id='sortable_list_box__list']//div[@class='item_box tr']/div[@class='item_pattern td']/a[contains(text(),'${pageName}')]/parent::node()/parent::node()/div[@class='icon_edit td']/div/a"]);
+        $this->tester->click(['xpath' => "//*[@id='sortable_list_box__list']//div[@class='item_box tr']/div[@class='item_pattern td']/a[contains(text(),'${pageName}')]/parent::node()/parent::node()/div[@class='icon_edit td']/div/ul/li[2]/a"]);
     }
 }
