2097f0e298dade528b2e6a5efdc4454b791c5e08
Chihiro Adachi <8196725+chihiro-adachi@users.noreply.github.com>
2018-03-08 15:20:42 +0900

    ページ管理のテストの修正

--- tests/_support/Page/Admin/PageManagePage.php
+++ tests/_support/Page/Admin/PageManagePage.php
@@ -4,7 +4,7 @@
 namespace Page\Admin;
 
 
-class PageManagePage extends AbstractAdminPage
+class PageManagePage extends AbstractAdminPageStyleGuide
 {
 
     /**
@@ -18,18 +18,17 @@ class PageManagePage extends AbstractAdminPage
     public static function go($I)
     {
         $page = new self($I);
-        return $page->goPage('/content/page', 'コンテンツ管理ページ管理');
+        return $page->goPage('/content/page', 'ページ管理コンテンツ管理');
     }
 
     public function 新規入力()
     {
-        $this->tester->click('#main > div > div > div > div.row.btn_area2 > div > a');
+        $this->tester->click(['xpath' => '//a[text()="新規追加"]']);
     }
 
     public function ページ編集($pageName)
     {
-        $this->tester->click(['xpath' => "//*[@id='sortable_list_box__list']//div[@class='item_box tr']/div[@class='item_pattern td']/a[contains(text(),'${pageName}')]/parent::node()/parent::node()/div[@class='icon_edit td']/div/a"]);
-        $this->tester->click(['xpath' => "//*[@id='sortable_list_box__list']//div[@class='item_box tr']/div[@class='item_pattern td']/a[contains(text(),'${pageName}')]/parent::node()/parent::node()/div[@class='icon_edit td']/div/ul/li[1]/a"]);
+        $this->tester->click(['xpath' => sprintf('//a[text()="%s"]', $pageName)]);
     }
 
     public function 削除($pageName)
