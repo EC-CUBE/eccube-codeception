26091f12935769aff42dc803388056473630cd8f
kazumi_watanabe <kazumi_watanabe@lockon.co.jp>
2019-05-15 11:55:36 +0900

    EA04OrderCest修正

--- tests/acceptance/EA04OrderCest.php
+++ tests/acceptance/EA04OrderCest.php
@@ -230,7 +230,7 @@ class EA04OrderCest
 
         codecept_debug($I->grabTextFrom('#template-change'));
 
-        $I->selectOption(['id' => 'template-change'], 'ご注文ありがとうございます');
+        $I->selectOption(['id' => 'template-change'], ['1' => 'ご注文ありがとうございます']);
         $I->click(['css' => '#button_box__button_menu > button']);
         $I->click(['css' => '#confirm_box__button_menu > p:nth-child(2) > button']);
 
@@ -260,7 +260,7 @@ class EA04OrderCest
             ->一覧_全選択()
             ->メール一括通知();
 
-        $I->selectOption(['id' => 'template-change'], 'ご注文ありがとうございます');
+        $I->selectOption(['id' => 'template-change'], ['1' => 'ご注文ありがとうございます']);
         $I->click(['css' => '#top_box__button_menu > button']);
         $I->click(['css' => '#confirm_box__button_menu > p:nth-child(2) > button']);
 
